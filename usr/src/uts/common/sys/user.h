db88e661987bcf1c12a9c67fcae72bd3cecb14e8
Robert Mustacchi <rm@joyent.com>
2013-06-15 13:35:34 -0700

    Add a preliminary number for arm auxv.
    While we don't quite know the total number of auxv fields that we're going to
    use for arm this lets us move forward with other aspects of the system. When we
    come back to the auxv values for userland/kernel we'll revise this number.

--- usr/src/uts/common/sys/user.h
+++ usr/src/uts/common/sys/user.h
@@ -26,7 +26,7 @@
 /*	Copyright (c) 1984, 1986, 1987, 1988, 1989 AT&T	*/
 /*	  All Rights Reserved	*/
 /*
- * Copyright (c) 2012 Joyent, Inc.  All rights reserved.
+ * Copyright (c) 2013 Joyent, Inc.  All rights reserved.
  */
 
 
@@ -188,6 +188,8 @@ typedef struct {		/* kernel syscall set type */
 #define	__KERN_NAUXV_IMPL 20
 #elif defined(__i386) || defined(__amd64)
 #define	__KERN_NAUXV_IMPL 22
+#elif defined(__arm__)
+#define	__KERN_NAUXV_IMPL 20	/* XXX TOTAL GUESS */
 #endif
 
 struct execsw;
