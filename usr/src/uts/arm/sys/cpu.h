4ff452bb27275580a4e02a5d462738dabe5bbd0a
Robert Mustacchi <rm@joyent.com>
2013-06-27 00:40:35 -0700

    Continue the ARM header stubbing exercises

---
+++ usr/src/uts/arm/sys/cpu.h
@@ -0,0 +1,30 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright (c) 2013 Joyent, Inc.  All rights reserved.
+ */
+
+#ifndef _SYS_CPU_H
+#define	_SYS_CPU_H
+
+/*
+ * This header is generally Obsolete across different architectures. We should
+ * include the bare minimum for compatability and nothing more. If you're adding
+ * something here, think twice about that.
+ */
+
+/*
+ * Include generic bustype cookies.
+ */
+#include <sys/bustypes.h>
+
+#endif /* _SYS_CPU_H */
---
+++ usr/src/uts/armv6/sys/x_call.h
@@ -0,0 +1,36 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright (c) 2013, Joyent, Inc.  All rights reserved.
+ */
+
+#ifndef _PROTOTYPE_H
+#define	_PROTOTYPE_H
+
+/*
+ * ARMv6 Cross call definitions
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * XXXARM Cross calls will be great to have, but for now we're punting before
+ * first down.
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PROTOTYPE_H */
