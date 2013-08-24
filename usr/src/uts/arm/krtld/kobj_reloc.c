406f413bfbd27a4885b36e60173f99115e265cb4
Robert Mustacchi <rm@joyent.com>
2013-08-24 11:32:00 -0700

    Implement kobj_reloc.c relocation support
    This implements all of the support for relocationg kernel modules that krtld
    needed that isn't a part of the generic doreloc.c shared pieces. We've opted not
    to bother at all with tnf. tnf probes showing up will be unresolved symbols --
    we don't really have much reason to implement it on ARM. SDT support is stubbed
    out at this time.

--- usr/src/uts/arm/krtld/kobj_reloc.c
+++ usr/src/uts/arm/krtld/kobj_reloc.c
@@ -1,12 +1,26 @@
 /*
- * This file and its contents are supplied under the terms of the
- * Common Development and Distribution License ("CDDL"), version 1.0.
- * You may only use this file in accordance with the terms of version
- * 1.0 of the CDDL.
+ * CDDL HEADER START
  *
- * A full copy of the text of the CDDL should have accompanied this
- * source.  A copy of the CDDL is also available via the Internet at
- * http://www.illumos.org/license/CDDL.
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
  */
 
 /*
@@ -28,6 +42,7 @@
 static int
 sdt_reloc_resolve(struct module *mp, char *symname, uint8_t *instr)
 {
+	_kobj_printf(ops, "sdt_reloc_resolve: Implement me\n");
 	return (1);
 }
 
@@ -35,13 +50,192 @@ int
 do_relocate(struct module *mp, char *reltbl, Word relshtype, int nreloc,
     int relocsize, Addr baseaddr)
 {
-	_kobj_printf(ops, "Implement me\n");
-	return (-1);
+	int symnum;
+	int err = 0;
+	unsigned long off, stndx, reladdr, rend, rtype;
+	long value;
+	Sym *symref;
+
+	reladdr = (unsigned long)reltbl;
+	rend = reladdr + nreloc * relocsize;
+
+#ifdef	KOBJ_DEBUG
+	if (kobj_debug & D_RELOCATIONS) {
+		_kobj_printf(ops, "krtld:\ttype\t\t\toffset      symbol\n");
+		_kobj_printf(ops, "krtld:\t\t\t\t\t   value\n");
+	}
+#endif
+
+	while (reladdr < rend) {
+		symnum++;
+		rtype = ELF32_R_TYPE(((Rel *)reladdr)->r_info);
+		off = ((Rel *)reladdr)->r_offset;
+		stndx = ELF32_R_SYM(((Rel *)reladdr)->r_info);
+		if (stndx >= mp->nsyms) {
+			_kobj_printf(ops, "do_relocate: bad strndx %d\n",
+			    symnum);
+			return (-1);
+		}
+		if ((rtype > R_ARM_NUM) || IS_TLS_INS(rtype)) {
+			_kobj_printf(ops, "krtld: invalid relocation type %d",
+			    rtype);
+			_kobj_printf(ops, " at 0x%llx:", off);
+			_kobj_printf(ops, " file=%s\n", mp->filename);
+			err = 1;
+			continue;
+		}
+
+		reladdr += relocsize;
+
+		if (rtype == R_ARM_NONE)
+			continue;
+
+#ifdef	KOBJ_DEBUG
+		if (kobj_debug & D_RELOCATIONS) {
+			Sym *	symp;
+			symp = (Sym *)
+			    (mp->symtbl+(stndx * mp->symhdr->sh_entsize));
+			_kobj_printf(ops, "krtld:\t%s",
+			    conv_reloc_ARM_type(rtype));
+			_kobj_printf(ops, "\t0x%8x", off);
+			_kobj_printf(ops, "  %s\n",
+			    (const char *)mp->strings + symp->st_name);
+		}
+#endif
+
+		if (!(mp->flags & KOBJ_EXEC))
+			off += baseaddr;
+
+		/*
+		 * XXX ia32 looks for R_386_RELATIVE, but R_ARM_RELATIVE is
+		 * not supported by us. Seems like we can probably ignore it for
+		 * now?
+		 */
+		/*
+		 * get symbol table entry - if symbol is local
+		 * value is base address of this object
+		 */
+		symref = (Sym *)(mp->symtbl+(stndx * mp->symhdr->sh_entsize));
+
+		if (ELF32_ST_BIND(symref->st_info) == STB_LOCAL) {
+			/* *** this is different for .o and .so */
+			value = symref->st_value;
+		} else {
+			/*
+			 * It's global. Allow weak references.  If
+			 * the symbol is undefined, give TNF (the
+			 * kernel probes facility) a chance to see
+			 * if it's a probe site, and fix it up if so.
+			 */
+			if (symref->st_shndx == SHN_UNDEF &&
+			    sdt_reloc_resolve(mp, mp->strings + symref->st_name,
+			    (uint8_t *)off) == 0)
+				continue;
+
+			/*
+			 * Traditionally you would also check for tnf here.
+			 */
+			if (symref->st_shndx == SHN_UNDEF) {
+				if (ELF32_ST_BIND(symref->st_info) !=
+				    STB_WEAK) {
+					_kobj_printf(ops, "not found: %s\n",
+					    mp->strings + symref->st_name);
+					err = 1;
+				}
+				continue;
+			} else { /* symbol found  - relocate */
+
+				/*
+				 * calculate location of definition - symbol
+				 * value plus base address of containing shared
+				 * object
+				 */
+				value = symref->st_value;
+
+			} /* end else symbol found */
+		} /* end global or weak */
+
+		/*
+		 * calculate final value -
+		 * if PC-relative, subtract ref addr
+		 */
+		if (IS_PC_RELATIVE(rtype))
+			value -= off;
+
+#ifdef	KOBJ_DEBUG
+		if (kobj_debug & D_RELOCATIONS) {
+			_kobj_printf(ops, "krtld:\t\t\t\t0x%8x", off);
+			_kobj_printf(ops, " 0x%8x\n", value);
+		}
+#endif
+
+		if (do_reloc_krtld(rtype, (unsigned char *)off, (Word *)&value,
+		    (const char *)mp->strings + symref->st_name,
+		    mp->filename) == 0)
+			err = 1;
+
+	} /* end of while loop */
+	if (err)
+		return (-1);
+
+	/* TODO Traditional TNF splice probes */
+
+	return (0);
 }
 
 int
 do_relocations(struct module *mp)
 {
+	int scn, nreloc;
+	Shdr *s, *shp;
 	_kobj_printf(ops, "Implement me\n");
+	for (scn = 1; scn < mp->hdr.e_shnum; scn++) {
+		s = (Shdr *)(mp->shdrs + scn * mp->hdr.e_shentsize);
+		/* We don't support RELA on ARM */
+		if (s->sh_type == SHT_RELA) {
+			_kobj_printf(ops, "do_relocations: encountered RELA "
+			    "in %s\n", mp->filename);
+			return (-1);
+		}
+		/* If it's anything other than a REL section, ignore it */
+		if (s->sh_type != SHT_REL)
+			continue;
+		/* Only relocate the default symbol table */
+		if (s->sh_link != mp->symtbl_section) {
+			_kobj_printf(ops, "do_relocations: asked to relocate "
+			    "non-default symbol table: %s\n", mp->filename);
+			return (-1);
+		}
+		if (s->sh_info >= mp->hdr.e_shnum) {
+			_kobj_printf(ops, "do_relocations: %s sh_info ",
+			    mp->filename);
+			_kobj_printf(ops, "out of range %d\n", scn);
+			goto bad;
+		}
+		nreloc = s->sh_size / s->sh_entsize;
+		/* Get the section header for this relocation table */
+		shp = (Shdr *)(mp->shdrs + s->sh_info * mp->hdr.e_shentsize);
+
+		/*
+		 * Ignore anything that's not going to be loaded into memory.
+		 */
+		if (!(shp->sh_flags & SHF_ALLOC))
+			continue;
+
+		if (do_relocate(mp, (char *)s->sh_addr, s->sh_type, nreloc,
+		    s->sh_entsize, shp->sh_addr) < 0) {
+			_kobj_printf(ops,
+			    "do_relocations: %s do_relocate failed\n",
+			    mp->filename);
+			goto bad;
+		}
+		kobj_free((void *)s->sh_addr, s->sh_size);
+		s->sh_addr = 0;
+	}
+	mp->flags |= KOBJ_RELOCATED;
+	return (0);
+bad:
+	kobj_free((void *)s->sh_addr, s->sh_size);
+	s->sh_addr = 0;
 	return (-1);
 }
