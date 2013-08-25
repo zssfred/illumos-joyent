4b664af2a6e95d7bc1876fcacf7bb045fbe60ed6
Robert Mustacchi <rm@joyent.com>
2013-08-25 11:35:33 -0700

    Add a rudimentary qemu versatilepb target
    For now the versaitlepb target that we're using is specific to QEMU. This is
    because QEMU apparently doesn't actually implement flow control on its uarts.
    Likely this is the only real difference and we should long term merge this with
    a traditional versatilepb board.

---
+++ usr/src/uts/armv6/qvpb/boot_console.c
@@ -0,0 +1,61 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright (c) 2013 Joyent, Inc.  All rights reserved.
+ */
+
+/*
+ * QEMU versatilepb boot console implementation
+ *
+ * QEMU's versatilepb board allows for up to three different serial consoles
+ * based on the PrimeCell UART (PL011). Apparently it doesn't implement flow
+ * control so all we're supposed to do is read and write to the memory address
+ * to interact with it. Sigh.
+ */
+static volatile unsigned int *bcons_uart0addr = (void *)0x101f1000;
+
+/*
+ * For now, we only support uart0
+ */
+void
+bcons_init(char *bstr)
+{
+	return;
+}
+
+void
+bcons_putchar(int c)
+{
+	*bcons_uart0addr = c & 0x7f;
+	if (c == '\n')
+		bcons_putchar('\r');
+}
+
+void
+bcons_puts(const char *str)
+{
+	const char *c = str;
+	while (*c != '\0')
+		bcons_putchar(*c++);
+}
+
+int
+bcons_getchar(void)
+{
+	return (*bcons_uart0addr & 0x7f);
+}
+
+int
+bcons_ischar(void)
+{
+	return (1);
+}
---
+++ usr/src/uts/armv6/qvpb/locore.s
@@ -0,0 +1,89 @@
+/*
+ * This file and its contents are supplied under the terms of the
+ * Common Development and Distribution License ("CDDL"), version 1.0.
+ * You may only use this file in accordance with the terms of version
+ * 1.0 of the CDDL.
+ *
+ * A full copy of the text of the CDDL should have accompanied this
+ * source.  A copy of the CDDL is also available via the Internet at
+ * http://www.illumos.org/license/CDDL.
+ */
+
+/*
+ * Copyright 2013 (c) Joyent, Inc. All rights reserved.
+ */
+
+#include <sys/asm_linkage.h>
+
+/*
+ * Every story needs a beginning. This is ours.
+ */
+
+/*
+ * We are in a primordial world here. The BMC2835 is going to come along and
+ * boot us at _start. Normally we would go ahead and use a main() function, but
+ * for now, we'll do that ourselves. As we've started the world, we also need to
+ * set up a few things about us, for example our stack pointer. To help us out,
+ * it's useful to remember the rough memory map. Remember, this is for physcial
+ * addresses. There is no virtual memory here. These sizes are often manipulated
+ * by the 'configuration' in the bootloader.
+ *
+ * +----------------+ <---- Max physical memory
+ * |                |
+ * |                |
+ * |                |
+ * +----------------+
+ * |                |
+ * |      I/O       |
+ * |  Peripherals   |
+ * |                |
+ * +----------------+ <---- I/O base 0x20000000 (corresponds to 0x7E000000)
+ * |                |
+ * |     Main       |
+ * |    Memory      |
+ * |                |
+ * +----------------+ <---- Top of SDRAM
+ * |                |
+ * |       VC       |
+ * |     SDRAM      |
+ * |                |
+ * +----------------+ <---- Split determined by bootloader config
+ * |                |
+ * |      ARM       |
+ * |     SDRAM      |
+ * |                |
+ * +----------------+ <---- Bottom of physical memory 0x00000000
+ *
+ * With the Raspberry Pi Model B, we have 512 MB of SDRAM. That means we have a
+ * range of addresses from [0, 0x20000000). If we assume that the minimum amount
+ * of DRAM is given to the GPU - 32 MB, that means we really have the following
+ * range: [0, 0x1e000000).
+ *
+ * By default, this binary will be loaded into 0x8000. For now, that means we
+ * will set our initial stack to 0x10000000.
+ */
+
+/*
+ * Recall that _start is the traditional entry point for an ELF binary.
+ */
+	ENTRY(_start)
+	mov sp,#0x8000
+        /*
+         * XXX Currently we're using u-boot to allow us to make forward progress
+         * while the .data section is a bit tumultuous. It loads that, but we
+         * can say for certain that it does not correctly pass in the machid and
+         * tagstart. Since we know what it is, we manually fix it up here.
+         */
+	mov r2,#0x100
+	bl _fakebop_start
+	SET_SIZE(_start)
+
+	ENTRY(arm_reg_read)
+	ldr r0, [r0]
+	bx lr
+	SET_SIZE(arm_reg_read)
+
+	ENTRY(arm_reg_write)
+	str r1, [r0]
+	bx lr	
+	SET_SIZE(arm_reg_write)
