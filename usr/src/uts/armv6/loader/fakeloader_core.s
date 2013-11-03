64b35356d2093413b85db0517f2a17ba30ba0623
Robert Mustacchi <rm@joyent.com>
2013-11-02 23:27:28 -0700

    atags are at 0x100, not 0t100

--- usr/src/uts/armv6/loader/fakeloader_core.s
+++ usr/src/uts/armv6/loader/fakeloader_core.s
@@ -103,7 +103,7 @@ fakeload_mmu_enable(void)
 
 	ENTRY(fakeload_exec)
 	mov	r4, r0
-	mov	r2, #100
+	mov	r2, #0x100
 	blx	r4
 	/* We should never execute this. If we do we'll go back to a panic */
 	bx	lr
