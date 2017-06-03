6ee415a96b5f740aa6b04174a0a2adc5a14769d6
Jason King <jason.brian.king@gmail.com>
2017-06-03 01:35:43 -0500

    Added in timer system using libuutil

---
+++ usr/src/cmd/cmd-inet/usr.lib/in.ikev2/common/timer.c
@@ -0,0 +1,267 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2010 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#include <pthread.h>
+#include <errno.h>
+#include <port.h>
+#include <string.h>
+#include <sys/types.h>
+#include <libuutil.h>
+#include <sys/debug.h>
+#include <umem.h>
+
+#include "timer.h"
+
+typedef struct tevent_s {
+	uu_list_node_t	node;
+
+	hrtime_t	time;	/* When does the event go off */
+	te_event_t	type;	/* Event type */
+
+	void		tevent_cb_fn;
+	void		*arg;
+} tevent_t;
+
+#define	TIMER_HEAD	((tevent_t *)uu_list_first(timer_list))
+
+static uu_list_pool_t 		*timer_pools;
+static __thread uu_list_t	*timer_list;
+static umem_cache_t		*evt_cache;
+
+static int te_compare(void *, void *, void *);
+
+static tevent_t *tevent_alloc(te_event_t, hrtime_t, tevent_cb_fn, void *);
+static void tevent_free(teven_t *);
+static int evt_ctor(void *, void *, int);
+static void evt_dtor(void *, void *);
+
+void
+ike_timer_init(void)
+{
+
+	uint32_t flg = 0;
+
+#ifdef DEBUG
+	flg |= UU_LIST_POOL_DEBUG;
+#endif
+
+	/* better be single threaded here! */
+	ASSERT(pthread_self() == 1);
+
+	VERIFY((timer_pools = uu_list_pool_create("timer lists",
+	    sizeof (tevent_t), offsetof(tevent_t, node), te_compare,
+	    flg)) != NULL);
+
+	evt_cache = umem_cache_create("timer events", sizeof (tevent_t),
+	    sizeof (size_t), evt_ctor, evt_dtor, NULL, NULL, 0);
+
+}
+
+/*
+ * Called for each new worker thread
+ */
+void
+ike_timer_thread_init(void)
+{
+	uint32_t flg = UU_LIST_SORTED;
+
+#ifdef DEBUG
+	flg |= UU_LIST_DEBUG;
+#endif
+
+	VERIFY((timer_list = uu_list_create(timer_pool, NULL, flg)) != NULL);
+}
+
+static int dispatch_cb(void *, void *);
+
+void
+process_timer(timespec_t *next_time)
+{
+	tevent_t *te;
+	hrtime_t now;
+
+	while (1) {
+		/*
+		 * since dispatching takes a non-zero amount of time, it is
+		 * possible that by the time we're done dispatching, new
+		 * events are due.  Eventually the list will either drain
+		 * or we are left with an event far enough in the future
+		 * that it's still pending after we're done dispatching
+		 */
+		now = gethrtime();
+
+		if ((te = TIMER_HEAD) == NULL) {
+			next_time->tv_sec = thread_timeout;
+			next_time->tv_nsec = 0;
+			return;
+		}
+
+		if (te->time > now) {
+			/* no events to run */
+			hrtime_t delta = te->time - now;
+
+			next_time->tv_sec = delta / (hrtime_t)NANOSEC;
+			next_time->tv_nsec = delta % (hrtime_t)NANOSEC;
+			return;
+		}
+
+		/* dispatch timeouts */
+		uu_list_walk(timer_list, dispatch_cb, *now, UU_WALK_ROBUST);	
+	}
+}
+
+static int
+dispatch_cb(void *elem, void *arg)
+{
+	tevent_t *te = (tevent_t *)elem;
+	const hrtime_t *now = (const hrtime_t *)arg;
+
+	if (te->time > *now)
+		return (UU_WALK_DONE);
+
+	te->fn(te->type, te->arg);
+	uu_list_remove(timer_list, elem);
+	tevent_free(te);
+
+	return (UU_WALK_CONTINUE);	
+}
+
+typedef struct cancel_arg_s {
+	te_event_t	type;
+	void		*arg;
+	size_t		n;
+} cancel_arg_t;
+
+static int cancel_cb(void *, void *);
+
+int
+cancel_timeout(te_event_t type, void *arg)
+{
+	cancel_arg_t carg;
+
+	carg.type = type;
+	carg.arg = arg;
+	carg.n = 0;
+
+	(void) uu_list_walk(timer_list, cancel_cb, &carg, UU_WALK_ROBUST);
+	return (carg.n);
+}
+
+static int
+cancel_cb(void *elem, void *arg)
+{
+	tevent_t *te = (tevent_t *)elem;
+	cancel_arg_t *carg = (cancel_arg_t *)arg;
+
+	if (carg->type == TE_ANY ||
+	    ((carg->type == te->type) && (carg->arg == te->arg))) {
+		uu_list_remove(timer_list, elem);
+		tevent_free(te);
+		carg->n++;
+	}
+	return (UU_WALK_CONTINUE);
+}
+
+boolean_t
+schedule_timeout(te_event_t type, tevent_cb_fn fn, void *arg, hrtime_t val)
+{
+	tevent_t *te; = tevent_alloc(type, val, fn, arg);
+	tevent_t *node;
+	uu_list_index_t idx;
+
+	VERIFY(te != TE_ANY);
+
+	if ((te = tevent_alloc(type, val, fn, arg)) == NULL)
+		return (B_FALSE);
+
+	(void) uu_list_find(timer_list, te, NULL, &idx);
+	uu_list_insert(timer_list, te, idx);
+	return (B_TRUE);
+}
+
+static int
+te_compare(void *la, void *ra, void *dummy)
+{
+	tevent_t *l = (tevent_t *)la;
+	tevent_t *r = (tevent_t *)ra;
+
+	if (l->time > r->time)
+		return (1);
+	if (l->time < r->time)
+		return (-1);
+	return (0);
+}
+
+static tevent_t *
+tevent_alloc(te_event_t type, hrtime_t dur, tevent_cb_fn fn, void *arg)
+{
+	tevent_t *te = umem_cache_alloc(evt_cache, UMEM_DEFAULT);
+
+	if (te == NULL)
+		return (NULL);
+
+	te->time = gethrtime() + dur;
+	te->type = type;
+	te->fn = fn;
+	te->arg = arg;
+
+	return (te);
+}
+
+static void
+tevent_free(teven_t *te)
+{
+	if (te == NULL)
+		return;
+
+	te->time = 0;
+	te->type = 0;
+	te->fn = NULL;
+	te->arg = NULL;
+
+	umem_cache_free(evt_cache, te);
+}
+
+static int
+evt_ctor(void *buf, void *cb, int flags)
+{
+	tevent_t *te = (tevent_t *)buf;
+
+	(void) memset(te, 0, sizeof (*te));
+	uu_list_node_init(buf, &te->node, timer_pools);
+
+	return (0);
+}
+
+static void
+evt_dtor(void *buf, void *cb)
+{
+	tevent_t *te = (tevent_t *)buf;
+
+	uu_list_node_fini(buf, &te->node, timer_pools);
+	(void) memset(te, 0, sizeof (*te));
+}
+
---
+++ usr/src/cmd/cmd-inet/usr.lib/in.ikev2/common/timer.h
@@ -0,0 +1,63 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2010 Sun Microsystems, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+/*
+ * Timer events are sorted into a single mutex-protected list.
+ * They are insertion-sorted by next-to-expire.
+ */
+
+#ifndef _IKEV2_TIMER_H
+#define _IKEV2_TIMER_H
+
+#include <sys/types.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+        TE_TEST = -1,   /* For code testing only, arg == NULL */
+        TE_ANY,         /* MUST NOT be passed to schedule_timeout() */
+        TE_SA_EXPIRE,   /* SA expiration */
+        TE_COOKIE_GEN,  /* Cookie generation */
+        TE_TRANSMIT,    /* Transmit timeout */
+        TE_PFKEY        /* pfkey timeout */
+} te_event_t;
+
+typedef void (*tevent_cb_fn)(te_event_t, void *);
+
+extern void		process_timer(timespec_t *);
+extern int		cancel_timeout(te_event_t, void *);
+extern boolean_t	schedule_timeout(te_event_t, tevent_cb_fn, void *,
+    hrtime_t);
+
+extern void ikev2_timer_init(void);
+extern void ikev2_timer_thread_init(void);
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif  /* _IKEV2_TIMER_H */
