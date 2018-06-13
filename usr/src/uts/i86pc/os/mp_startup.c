52b3cf5cd58128b1ae465be28a32b1cc5c617a3f
John Levon <john.levon@joyent.com>
2018-06-13 22:54:04 +0000

    OS-6967 LDT still not happy under KPTI Reviewed by: Robert Mustacchi <rm@joyent.com> Reviewed by: Jerry Jelinek <jerry.jelinek@joyent.com> Reviewed by: Alex Wilson <alex.wilson@joyent.com> Approved by: Jerry Jelinek <jerry.jelinek@joyent.com> Approved by: Alex Wilson <alex.wilson@joyent.com>

--- usr/src/uts/i86pc/os/mp_startup.c
+++ usr/src/uts/i86pc/os/mp_startup.c
@@ -2040,9 +2040,8 @@ mp_cpu_faulted_exit(struct cpu *cp)
  * syscall features.
  */
 
-/*ARGSUSED*/
 void
-cpu_fast_syscall_disable(void *arg)
+cpu_fast_syscall_disable(void)
 {
 	if (is_x86_feature(x86_featureset, X86FSET_MSR) &&
 	    is_x86_feature(x86_featureset, X86FSET_SEP))
@@ -2052,9 +2051,8 @@ cpu_fast_syscall_disable(void *arg)
 		cpu_asysc_disable();
 }
 
-/*ARGSUSED*/
 void
-cpu_fast_syscall_enable(void *arg)
+cpu_fast_syscall_enable(void)
 {
 	if (is_x86_feature(x86_featureset, X86FSET_MSR) &&
 	    is_x86_feature(x86_featureset, X86FSET_SEP))
