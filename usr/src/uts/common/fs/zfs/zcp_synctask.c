8080e324cb82f1a14372f0296c0d68242cadf7b5
Jason King <jason.king@joyent.com>
2019-08-10 03:40:42 +0000

    Add zfs.sync.set_prop channel program

--- usr/src/uts/common/fs/zfs/zcp_synctask.c
+++ usr/src/uts/common/fs/zfs/zcp_synctask.c
@@ -15,6 +15,7 @@
 
 /*
  * Copyright (c) 2016, 2017 by Delphix. All rights reserved.
+ * Copyright 2019 Joyent, Inc.
  */
 
 #include "lua.h"
@@ -275,6 +276,63 @@ zcp_synctask_snapshot(lua_State *state, boolean_t sync, nvlist_t *err_details)
 	return (err);
 }
 
+static int zcp_synctask_set_prop(lua_State *, boolean_t, nvlist_t *);
+static zcp_synctask_info_t zcp_synctask_set_prop_info = {
+	.name = "set_prop",
+	.func = zcp_synctask_set_prop,
+	.pargs = {
+		/*
+		 * While zfs(1) does 'zfs set prop=value... dataset,
+		 * here we do dataset as the first argument.  The other
+		 * lua zfs.sync commands follow this format, so we choose
+		 * to be consistent with them.
+		 *
+		 * XXX: How to handle non-string properties? Should we
+		 * allow a table to specify a group of properties?
+		 * Should that be a separate command?
+		 */
+		{ .za_name = "dataset", .za_lua_type = LUA_TSTRING },
+		{ .za_name = "property", .za_lua_type = LUA_TSTRING },
+		{ .za_name = "value", .za_lua_type = LUA_TSTRING },
+		{ NULL, 0 }
+	},
+	.kwargs = {
+		{ NULL, 0 }
+	},
+	/*
+	 * This follows the same settings used by dsl_props_set() in
+	 * dsl_prop.c
+	 */
+	.space_check = ZFS_SPACE_CHECK_RESERVED,
+	.blocks_modified = 2	/* 2 * numprops  */
+};
+
+static int
+zcp_synctask_set_prop(lua_State *state, boolean_t sync, nvlist_t *err_details)
+{
+	int err;
+	dsl_props_set_arg_t dpsa = { 0 };
+	const char *dsname = lua_tostring(state, 1);
+	const char *prop = lua_tostring(state, 2);
+	const char *val = lua_tostring(state, 3);
+
+	dpsa.dpsa_dsname = dsname;
+	dpsa.dpsa_source = ZPROP_SRC_LOCAL;
+	dpsa.dpsa_props = fnvlist_alloc();
+	fnvlist_add_string(dpsa.dpsa_props, prop, val);
+
+	zcp_cleanup_handler_t *zch = zcp_register_cleanup(state,
+	    (zcp_cleanup_t *)&fnvlist_free, dpsa.dpsa_props);
+
+	err = zcp_sync_task(state, dsl_props_set_check, dsl_props_set_sync,
+	    &dpsa, sync, dsname);
+
+	zcp_deregister_cleanup(state, zch);
+	fnvlist_free(dpsa.dpsa_props);
+
+	return (err);
+}
+
 static int
 zcp_synctask_wrapper(lua_State *state)
 {
@@ -342,6 +400,8 @@ zcp_load_synctask_lib(lua_State *state, boolean_t sync)
 		&zcp_synctask_promote_info,
 		&zcp_synctask_rollback_info,
 		&zcp_synctask_snapshot_info,
+		&zcp_synctask_set_prop_info,
+		&zcp_synctask_change_key_info,
 		NULL
 	};
 
